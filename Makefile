f39638c8cb8e6bfde5059cda998ec602e53e1fdc
Marc Paterno <paterno@fnal.gov>
2023-02-15 16:15:14 -0600

    Very incomplete testing of DualNum5

--- Makefile
+++ Makefile
@@ -3,15 +3,18 @@ CXXFLAGS=-O3 -std=c++17 -Wall -Wextra -mfma
 
 .PHONY: test
 
-all:  test_dual_num2 multiply_3.s
+all:  test_dual_num1 test_dual_num5 multiply_3.s
 
 clean:
-	rm -f overload test_dual_num2 main.o multiply_3.o multiply_3.s
+	rm -f overload test_dual_num1 test_dual_num5 main.o multiply_3.o multiply_3.s
 
 main.o: main.cc
 	$(CXX) $(CXXFLAGS) -c $<
 
-test_dual_num2: test_dual_num1.cc dual_num1.hh main.o
+test_dual_num1: test_dual_num1.cc dual_num1.hh main.o
+	$(CXX) $(CXXFLAGS) -o $@ $< main.o
+
+test_dual_num5: test_dual_num5.cc dual_num5.hh main.o
 	$(CXX) $(CXXFLAGS) -o $@ $< main.o
 
 multiply_3.o: multiply_3.cc dual_num1.hh dual_num5.hh
@@ -20,6 +23,7 @@ multiply_3.o: multiply_3.cc dual_num1.hh dual_num5.hh
 multiply_3.s: multiply_3.o
 	objdump -d $< > $@
 
-test: test_dual_num2
-	./test_dual_num2
+test: test_dual_num1 test_dual_num5
+	./test_dual_num1
+	./test_dual_num5
 
---
+++ test_dual_num5.cc
@@ -0,0 +1,235 @@
+#include "catch.hpp"
+#include "dual_num5.hh"
+#include <array>
+#include <cstdio>
+#include <type_traits>
+
+TEST_CASE("construct DualNum5s")
+{
+  DualNum5 x;
+  CHECK(x(0) == 0.0);
+  CHECK(x(1) == 0.0);
+  CHECK(x(2) == 0.0);
+  CHECK(x(3) == 0.0);
+  CHECK(x(4) == 0.0);      
+
+  DualNum5 y(1.0);
+  CHECK(y(0) == 1.0);
+  CHECK(y(1) == 0.0);
+  CHECK(y(2) == 0.0);
+  CHECK(y(3) == 0.0);
+  CHECK(y(4) == 0.0);
+  
+  DualNum5 z(1.0, 2.0, 3.0, 4.0, 5.0);
+  CHECK(z(0) == 1.0);
+  CHECK(z(1) == 2.0);
+  CHECK(z(2) == 3.0);
+  CHECK(z(3) == 4.0);
+  CHECK(z(4) == 5.0);      
+}
+
+#if 0
+TEST_CASE("addition of DualNum5s")
+{
+  DualNum5 x(1.0, 2.0);
+  DualNum5 y(3.0, 4.0);
+  DualNum5 z = x + y;
+  CHECK(z(0) == 4.0);
+  CHECK(z(1) == 6.0);
+}
+
+TEST_CASE("subtraction of DualNum5s")
+{
+  DualNum5 x(1.0, 2.0);
+  DualNum5 y(3.0, 4.0);
+  DualNum5 w = x - y;
+  CHECK(w(0) == -2.0);
+  CHECK(w(1) == -2.0);
+}
+#endif
+
+TEST_CASE("multiplication of DualNum5s")
+{
+  DualNum5 x(1.0, 2.0, 3.0, 4.0, 5.0);
+  DualNum5 y(3.0, 4.0, 5.0, 6.0, 7.0);
+  DualNum5 z = x * y;
+  CHECK(z(0) == 3.0);
+  CHECK(z(1) == 10.0);
+  CHECK(z(2) == 14.0);
+  CHECK(z(3) == 18.0);
+  CHECK(z(4) == 22.0);
+}
+
+#if 0
+TEST_CASE("literal DualNum5s")
+{
+  auto x = 1.0_dn;
+  CHECK(x(0) == 1.0);
+  CHECK(x(1) == 0.0);
+
+  auto y = 3_dn;
+  CHECK(y(0) == 3.0);
+  CHECK(y(1) == 0.0);
+}
+
+TEST_CASE("add DualNum5 + double")
+{
+  DualNum5 x(1.0, -1.0);
+  DualNum5 z = x + 1.0;
+  CHECK(z(0) == 2.0);
+  CHECK(z(1) == -1.0);
+}
+
+TEST_CASE("add double + DualNum5")
+{
+  DualNum5 x(1.0, -1.0);
+  DualNum5 z = 1.0 + x;
+  CHECK(z(0) == 2.0);
+  CHECK(z(1) == -1.0);
+}
+
+TEST_CASE("multiply DualNum5 by double")
+{
+  DualNum5 x(2.0, 3.0);
+  DualNum5 z = x * 2.0;
+  CHECK(z(0) == 4.0);
+  CHECK(z(1) == 6.0);
+}
+
+TEST_CASE("multiply double by DualNum5")
+{
+  DualNum5 x(2.0, 3.0);
+  DualNum5 z = 3.0 * x;
+  CHECK(z(0) == 6.0);
+  CHECK(z(1) == 9.0);
+}
+
+TEST_CASE("mixed expression with DualNum5s")
+{
+  DualNum5 w(-1.0, -3.0);
+  DualNum5 x(1.0, 2.0);
+  DualNum5 y(3.0, 4.0);
+
+  SECTION("multiply then add")
+  {
+    // This is the value we want to check.
+    DualNum5 z = w * x + y;
+
+    // This is the expression evaluation broken down
+    // into pieces.
+    DualNum5 t1 = w * x;
+    DualNum5 expr = t1 + y;
+    CHECK(z == expr);
+  }
+
+  SECTION("add then multiply")
+  {
+    // This is the value we want to check.
+    DualNum5 z = w + x * y;
+
+    // This is the expression evaluation broken down
+    // into pieces.
+    DualNum5 t2 = x * y;
+    DualNum5 expr = w + t2;
+    CHECK(z == expr);
+  }
+
+  SECTION("respect these parentheses")
+  {
+    // This is the value we want to check.
+    DualNum5 z = (w + x) * y;
+
+    // This is the expression evaluation broken down into pieces.
+    DualNum5 t1 = w + x;
+    DualNum5 expr = t1 * y;
+    CHECK(z == expr);
+  }
+}
+
+TEST_CASE("addition expressions with bare doubles")
+{
+  DualNum5 x(-1.0, 1.0);
+  DualNum5 y(2.0, 3.0);
+
+  SECTION("double at end")
+  {
+    DualNum5 z = x + y + 2.0;
+    CHECK(z(0) == 3.0);
+    CHECK(z(1) == 4.0);
+  }
+
+  SECTION("double in middle")
+  {
+    DualNum5 z = x + 2.0 + y;
+    CHECK(z(0) == 3.0);
+    CHECK(z(1) == 4.0);
+  }
+
+  SECTION("double in the front")
+  {
+    DualNum5 z = 2.0 + x + y;
+    CHECK(z(0) == 3.0);
+    CHECK(z(1) == 4.0);
+  }
+}
+
+TEST_CASE("multiplication expressions with bare doubles")
+{
+  DualNum5 x(1.0, 2.0);
+  DualNum5 y(3.0, -1.0);
+
+  SECTION("double at end")
+  {
+    DualNum5 z = x * y * 3.0;
+
+    DualNum5 t1 = x * y;
+    DualNum5 expected = t1 * DualNum5(3.0);
+    CHECK(z == expected);
+  }
+
+  SECTION("double in the middle")
+  {
+    DualNum5 z = x * 3.0 * y;
+
+    DualNum5 t1 = x * 3.0;
+    DualNum5 expected = t1 * y;
+    CHECK(z == expected);
+  }
+
+  SECTION("double in the front")
+  {
+    DualNum5 z = 3.0 * x * y;
+
+    DualNum5 t1 = 3.0 * x;
+    DualNum5 expected = t1 * y;
+    CHECK(z == expected);
+  }
+}
+
+TEST_CASE("subtraction expressions with bare doubles")
+{
+  DualNum5 x(-1.0, 1.0);
+  DualNum5 y(2.0, 3.0);
+
+  SECTION("double at end")
+  {
+    DualNum5 z = x - y - 2.0;
+    CHECK(z(0) == -5.0);
+    CHECK(z(1) == -2.0);
+  }
+
+  SECTION("double in middle")
+  {
+    DualNum5 z = x - 2.0 - y;
+    CHECK(z(0) == -5.0);
+    CHECK(z(1) == -2.0);
+  }
+
+  SECTION("double in the front")
+  {
+    DualNum5 z = 2.0 - x - y;
+    CHECK(z(0) == 1.0);
+    CHECK(z(1) == -4.0);
+  }
+}
+#endif
