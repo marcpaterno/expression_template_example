6728a5508be709cbd47098ac4134edd4f7646909
Marc Paterno <paterno@fnal.gov>
2023-03-25 11:25:21 -0500

    Include generation of assembly code for benchmark functions

--- full_build.sh
+++ full_build.sh
@@ -2,9 +2,11 @@ for d in build-*;
 do 
   echo "******* Starting on $d";
   pushd $d;
+  # make clean;
   make -j;
   ctest | ctest --output-on-failure --rerun-failed;
-  # make fake_mult_3.s long_mult_3.s only_construct.s right_fold_mult_3.s use_mult_3.s;
+  make fake_mult_3.s long_mult_3.s only_construct.s right_fold_mult_3.s use_mult_3.s;
+  make benchmark_mult_3.s benchmark_only_construct.s
   echo "******* Done with $d";
   popd;
 done
